30c68bb311aeaef27e73487d2389f92e276fdc9c
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 11:39:09 +0430

    Use the card ui component in the availabe meals component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './AvailableMeals.module.css'
+import Card from '../UI/Card'
 
 const DUMMY_MEALS = [
     {
@@ -31,9 +32,12 @@ export default function AvailableMeals() {
     const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
     return (
         <section className={styles.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+
+                </ul>
+            </Card>
         </section>
     )
 }
