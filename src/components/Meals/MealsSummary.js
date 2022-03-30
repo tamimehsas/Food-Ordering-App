a0433e8c196b1116b33c1d2185b0f5f253241cc7
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 11:26:34 +0430

    Add the mealsSummary component markup

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from './MealsSummary.module.css'
 
 export default function MealsSummary() {
-  return (
-    <div>MealsSummary</div>
-  )
+    return (
+        <section className={styles.summary}>
+            <h2>Delicious food, delivered to you</h2>
+            <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
+            <p>All our meals are cooked with high quality ingredients, just-in-time and of course by experienced chefs</p>
+        </section>
+    )
 }
