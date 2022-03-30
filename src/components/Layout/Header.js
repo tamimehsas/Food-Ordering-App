14ccc662b5183d3b716485a22eb52494122aac16
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 12:45:59 +0430

    Pass props to the HeaderCart button component to display the cart while clicked

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import mealsImage from '../../assets/meals.jpg'
 import HeaderCartButton from './HeaderCartButton'
 import styles from './Header.module.css'
 
-export default function Header() {
+export default function Header(props) {
     return (
         <Fragment>
             <header className={styles.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart}/>
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt="A table full of delicious foods" />
