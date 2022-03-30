d4c69daa6fc80888dbabbe313b5a09818c1734ed
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 11:33:21 +0430

    Add the meals summary and availabe meals components to the meals component

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import MealsSummary from './MealsSummary'
+import AvailableMeals from './AvailableMeals'
 
 export default function Meals() {
-  return (
-    <div>Meals</div>
-  )
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvailableMeals />
+        </Fragment>
+    )
 }
