c83f46c9949c628ee51c1d359498f60343f8533a
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 12:53:49 +0430

    Add the logic to close the modal while the backdrop is clicked

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ export default function Cart(props) {
     const cartItems = <ul className={styles['cart-items']}>
         {[{ id: 'c1', name: 'sushi', price: 12.99 }].map(item => <li>{item.name}</li>)}</ul>
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={styles.total}>
                 <span>Total amount</span>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import styles from './Modal.module.css'
 
 const Backdrop = (props) => {
     return (
-        <div className={styles.backdrop}></div>
+        <div className={styles.backdrop} onClick={props.onClose}></div>
     )
 }
 
@@ -21,7 +21,7 @@ const ModalOverlay = (props) => {
 const portalElement  = document.getElementById('overlays')
 export default function Modal(props) {
     return <Fragment>
-        {ReactDOM.createPortal(<Backdrop />,portalElement)}
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)}
         {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
 
     </Fragment>
