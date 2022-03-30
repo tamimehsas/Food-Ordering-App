34a61a80d3db7d4b27c153ba0c60876251025231
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 12:11:46 +0430

    Use the input component in the MealItemform component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './MealItemForm.module.css'
+import Input from '../../UI/Input'
 
 export default function MealItemForm(props) {
   return (
     <form className={styles.form}>
-      <input type="text" />
+      <Input label="amount" input={{ id: 'amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1' }} />
       <button>+ Add</button>
     </form>
   )
