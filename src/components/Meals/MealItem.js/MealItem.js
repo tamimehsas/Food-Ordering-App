a2ede6191fd1067f45ab20e4a053dfda735285ce
Tamim Ehsas <tamimehsas99@gmail.com>
2022-03-30 11:45:17 +0430

    Style the mealItem component

--- src/components/Meals/MealItem.js/MealItem.js
+++ src/components/Meals/MealItem.js/MealItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './MealItem.module.css'
 
 export default function MealItem(props) {
     const price = `$${props.price.toFixed(2)}`
     return (
-        <li>
+        <li className={styles.meal}>
             <div>
                 <h3>{props.name}</h3>
-                <div>{props.description}</div>
-                <div>{price}</div>
+                <div className={styles.description}>{props.description}</div>
+                <div className={styles.price}>{price}</div>
             </div>
             <div>
                 meal item form
